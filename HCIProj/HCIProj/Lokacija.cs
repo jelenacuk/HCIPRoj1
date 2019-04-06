d5233fdac5d9998b6d069857f849aab015cc34e1
MilePrastalo <mile.prastalo97@gmail.com>
2019-04-06 16:42:19 +0200

    Broke many things, fixed few

---
+++ HCIProj/HCIProj/Lokacija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCIProj
+{
+    public class Lokacija
+    {
+        public string Naziv { get; set; }
+        public bool Neomiljena { get; set; }
+    }
+}
--- HCIProj/HCIProj/MainWindow.xaml.cs
+++ HCIProj/HCIProj/MainWindow.xaml.cs
@@ -14,22 +14,100 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Newtonsoft.Json;
 using System.Net;
-
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 namespace HCIProj
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string _trenutnaLokacija;
+        private int _minTemp;
+        private int _maxTemp;
+        private int _temp;
+        public ObservableCollection<string> Lokacije { get; set; }
+        public String TrenutnaLokacija {
+            get {
+                return _trenutnaLokacija;
+            }
+            set {
+                if (value != _trenutnaLokacija)
+                {
+                    _trenutnaLokacija = value;
+                    OnPropertyChanged("TrenutnaLokacija");
+                }
+            }
+        }
+        public int MinTemp {
+            get {
+                return _minTemp;
+            }
+            set {
+                if (value != _minTemp)
+                {
+                    _minTemp = value;
+                    OnPropertyChanged("MinTemp");
+                }
+            }
+        }
+        public int MaxTemp {
+            get {
+                return _maxTemp;
+            }
+            set {
+                if (value != _maxTemp)
+                {
+                    _maxTemp = value;
+                    OnPropertyChanged("MaxTemp");
+                }
+            }
+        }
+        public int Temp {
+            get {
+                return _temp;
+            }
+            set {
+                if (value != _temp)
+                {
+                    _temp = value;
+                    OnPropertyChanged("Temp");
+                }
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            Temp = 0;
+            MinTemp = 0;
+            MaxTemp = 0;
+            Lokacije = new ObservableCollection<string>();
+            Lokacija l1 = new Lokacija();
+            l1.Naziv = "Novi Sad";
+            l1.Neomiljena = false;
+            Lokacija l2 = new Lokacija();
+            l2.Naziv = "Beograd";
+            l2.Neomiljena = true;
+            Lokacije.Add("Novi Sad");
+            Lokacije.Add("Beograd");
         }
 
-     
+        #region PropertyChangedNotifier
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+                
+            }
+        }
 
-        private  void Load_CurrentWeather(object sender, RoutedEventArgs e)
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        private void Load_CurrentWeather(object sender, RoutedEventArgs e)
         {
 
             using (WebClient webClient = new WebClient())
@@ -48,10 +126,18 @@ namespace HCIProj
                 int min_temp = Convert.ToInt32(Convert.ToDouble(min) - 273.15);
                 int max_temp = Convert.ToInt32(Convert.ToDouble(max) - 273.15);
 
+                Temp = temp;
+                MinTemp = min_temp;
+                MaxTemp = max_temp;
             }
         }
 
         private void TabItem_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
