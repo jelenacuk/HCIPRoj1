95e86eec228685175868d41fe37f5c5c3f1c4f7d
jelenacuk <jelena.cuk.1997@gmail.com>
2019-04-06 14:17:34 +0200

    Ucitavanje podataka o trenutnoj temperaturi

--- HCIProj/HCIProj/MainWindow.xaml.cs
+++ HCIProj/HCIProj/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace HCIProj
 {
@@ -25,9 +27,33 @@ namespace HCIProj
             InitializeComponent();
         }
 
+     
+
+        private  void Load_CurrentWeather(object sender, RoutedEventArgs e)
+        {
+
+            using (WebClient webClient = new WebClient())
+            {
+                string url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8e17202912490c577a70504fd76979f3";
+                string json = webClient.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+                WeatherInfo.root output = result;
+
+                var cTemp = output.main.temp;
+                var min = output.main.temp_min;
+                var max = output.main.temp_max;
+
+                int temp = Convert.ToInt32(Convert.ToDouble(cTemp) - 273.15);
+                int min_temp = Convert.ToInt32(Convert.ToDouble(min) - 273.15);
+                int max_temp = Convert.ToInt32(Convert.ToDouble(max) - 273.15);
+
+            }
+        }
+
         private void TabItem_GotFocus(object sender, RoutedEventArgs e)
         {
-            
+
         }
     }
 }
---
+++ HCIProj/HCIProj/WeatherInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCIProj
+{
+    class WeatherInfo
+    {
+        
+       
+
+        public class main
+        {
+            public double temp { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+
+        }
+      
+
+        public class root
+        {
+            public string name { get; set; }
+            public main main { get; set; }
+        }
+
+    }
+}
